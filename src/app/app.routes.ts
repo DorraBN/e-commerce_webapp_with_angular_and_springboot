c07388df4beeae0bc9af4c7cffdbe97a525a0feb
DorraBN <dorransira18@gmail.com>
2024-09-07 21:25:21 +0100

    ajout login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddComponent } from './add/add.component';
+import { RegisterComponent } from './register/register.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
   { path: 'login', component: LoginComponent }, 
   { path: 'dash', component: DashboardComponent },
+  { path: 'add', component: AddComponent },
+  { path: 'reg', component: RegisterComponent },
  // Route par défaut pour afficher le HomeComponent
   // Ajoutez d'autres routes ici si nécessaire
 ];
