334b072c5344d283dc323fd614b8c5d4604b05cf
DorraBN <dorransira18@gmail.com>
2024-09-08 17:31:34 +0100

    add user backend

--- backend/src/test/java/com/project/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/project/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.project.online;
+package com.project.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
