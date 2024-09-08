b9ab1e44084004497e1fe7f1b429bd95d7442153
DorraBN <dorransira18@gmail.com>
2024-09-08 17:10:13 +0100

    ajout backend

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, ReactiveFormsModule],
+  
   templateUrl: './add.component.html',
-  styleUrl: './add.component.css'
+  styleUrls: ['./add.component.css']
 })
-export class AddComponent {
+export class AddComponent implements OnInit {
+  productForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      brand: ['', Validators.required],
+      stockQuantity: [0, Validators.required],
+      description: [''],
+      price: [0, Validators.required],
+      color: ['#000000'],
+      category: ['Tshirt'],
+      available: [false],
+      notAvailable: [false],
+    });
+  }
+
+  onSubmit() {
+    this.productService.addProduct(this.productForm.value).subscribe(
+      response => {
+        console.log('Produit ajouté avec succès', response);
+      },
+      error => {
+        console.error('Erreur lors de l\'ajout du produit', error);
+        alert('Une erreur est survenue lors de l\'ajout du produit.');
+      }
+    );
+  }
+  
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
-
 
 @Injectable({
   providedIn: 'root'
@@ -20,10 +19,9 @@ export class ProductService {
     return this.http.get<Product>(`${this.baseUrl}/${id}`);
   }
 
-  createProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
+  addProduct(product: any) {
+    return this.http.post(this.baseUrl, product);
   }
-
   updateProduct(id: number, product: Product): Observable<Product> {
     return this.http.put<Product>(`${this.baseUrl}/${id}`, product);
   }
